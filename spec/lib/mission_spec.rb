2ed207dea249ea88dbd4a89d9d62c027e57c3fa9
Ivan Zarea <zarea.ion@gmail.com>
2016-04-13 13:30:35 +0300

    We have our main failing test now

---
+++ spec/lib/mission_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+# NOTE: the squiggly heredoc requires Ruby >= 2.3
+# I've extracted them to constants for now
+
+MissionTestInput = <<INPUT
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+INPUT
+
+MissionTestOutput = <<OUTPUT
+1 3 N
+5 1 E
+OUTPUT
+
+describe Mission do
+  let(:input) {
+    @input
+  }
+
+  let(:output) {
+    @output
+  }
+
+  it "can compute the rovers' positions given the inputs" do
+    expect(subject.process(MissionTestInput)).to eq(MissionTestOutput)
+  end
+end
--- spec/lib/sample_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'Something useful' do
-  it 'does something' do
-    expect(2).not_to eq(1)
-  end
-end
